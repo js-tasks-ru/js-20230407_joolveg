e2dfd76d6a02d285206b5909606ee1a3ebf368dc
Maria Zakutey <joolveg@gmail.com>
2023-04-19 00:28:20 +0300

    реализация метода sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,17 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const newArr = [...arr];
 
+  newArr.sort((a, b) => {
+    let elem = a.localeCompare(b, ['ru', 'en'], { caseFirst: 'upper'});
+    if (param === 'asc') {elem *= 1;}
+    else if (param === 'desc') {elem *= -1;}
+    else {
+      throw new Error(`Упс ... Направление сортировки передано: ${param}`);
+    }
+    return elem;
+  });
+
+  return newArr;
 }
